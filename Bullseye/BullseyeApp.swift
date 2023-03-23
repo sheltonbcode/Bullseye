325d5e6d902fff69d61740baa29eb04512a753ed
Shelton Brown <96554952+sheltonbcode@users.noreply.github.com>
2023-03-23 17:09:41 -0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Shelton Brown on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
