877b68521f47abc25f274fbbe11b44eaf0874983
Shelton Brown <96554952+sheltonbcode@users.noreply.github.com>
2023-03-29 14:35:06 -0500

    Finished nesting and simplifying ContentView with SliderValue and additional structs for binding (this was a pretty difficult task as I had to resolve an error for closing out one of my views with proper indentation).

--- Bullseye/App/Views/ContentView.swift
+++ Bullseye/App/Views/ContentView.swift
@@ -3,7 +3,6 @@
 //  Bullseye App
 //
 //  Created by Shelton Brown on 3/23/23.
-//  This comment is for Alex!
 
 import SwiftUI
 
@@ -18,49 +17,8 @@ struct ContentView: View {
 				.ignoresSafeArea()
 			VStack {
 				InstructionsView(game: $game)
-				HStack {
-					Text("1")
-						.bold()
-						.foregroundColor(Color("TextColor"))
-					Slider(value: $sliderValue, in: 1.0...100.0)
-					Text("100")
-						.bold()
-						.foregroundColor(Color("TextColor"))
-				}
-				.padding()
-				Button("Hit me".uppercased()) {
-					alertIsVisible = true
-				}
-				.padding(20.0)
-				.background(
-					ZStack {
-						Color("ButtonColor")
-						LinearGradient(
-							gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]),
-							startPoint: .top, endPoint: .bottom)
-					}
-				)
-				.foregroundColor(.white)
-				.cornerRadius(21.0)
-				.bold()
-				.font(.title3)
-				.alert(
-					"Howdy!",
-					isPresented: $alertIsVisible,
-					actions: {
-						Button("Awesome") {
-							print("Alert closed")
-						}
-					},
-					message: {
-						let roundedValue = Int(sliderValue.rounded())
-						Text("""
-The slider's value is \(roundedValue).
- You scored \(game.points(sliderValue: roundedValue)) points this round!
-""")
-						.foregroundColor(Color("TextColor"))
-					}
-				)
+				SliderView(sliderValue: $sliderValue)
+				HitMeButton(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
 			}
 		}
 	}
@@ -78,13 +36,68 @@ struct InstructionsView: View {
 	}
 }
 
-
-struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-			.previewDevice("iPhone 14 Pro")
-		ContentView()
-			.preferredColorScheme(.dark)
-			.previewDevice("iPhone 14 Pro")
+struct SliderView: View {
+	@Binding var sliderValue: Double
+	
+	var body: some View {
+		HStack {
+			SliderLabelText(text: "1")
+			Slider(value: $sliderValue, in: 1.0...100.0)
+			SliderLabelText(text: "100")
+				.bold()
+				.foregroundColor(Color("TextColor"))
+		}
+		.padding()
 	}
 }
+
+struct HitMeButton: View {
+	@Binding var alertIsVisible: Bool
+	@Binding var sliderValue: Double
+	@Binding var game: Game
+	
+	var body: some View {
+		Button("Hit me".uppercased()) {
+			alertIsVisible = true
+		}
+		.padding(20.0)
+		.background(
+			ZStack {
+				Color("ButtonColor")
+				LinearGradient(
+					gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]),
+					startPoint: .top, endPoint: .bottom)
+			}
+		)
+		.foregroundColor(.white)
+		.cornerRadius(21.0)
+		.bold()
+		.font(.title3)
+		.alert(
+			"Howdy!",
+			isPresented: $alertIsVisible,
+			actions: {
+				Button("Awesome") {
+					print("Alert closed")
+				}
+			},
+			message: {
+				let roundedValue = Int(sliderValue.rounded())
+				Text("""
+The slider's value is \(roundedValue).
+You scored \(game.points(sliderValue: roundedValue)) points this round!
+""")
+				.foregroundColor(Color("TextColor"))
+			}
+		)
+	}
+}
+		struct ContentView_Previews: PreviewProvider {
+			static var previews: some View {
+				ContentView()
+					.previewDevice("iPhone 14 Pro")
+				ContentView()
+					.preferredColorScheme(.dark)
+					.previewDevice("iPhone 14 Pro")
+			}
+		}
