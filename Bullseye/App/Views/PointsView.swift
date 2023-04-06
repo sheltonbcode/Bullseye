70415cfba41c5cb3ac482480046fc0880336ff05
Shelton Brown <96554952+sheltonbcode@users.noreply.github.com>
2023-04-06 17:02:29 -0500

    Updated bindings PointsView and created a custom alert that refreshes the round and adds the score when "Start New Round" is pressed.

---
+++ Bullseye/App/Views/PointsView.swift
@@ -0,0 +1,49 @@
+//
+//  PointsView.swift
+//  Bullseye
+//
+//  Created by Shelton Brown on 4/6/23.
+//
+
+import SwiftUI
+
+struct PointsView: View {
+	@Binding var alertIsVisible: Bool
+	@Binding var sliderValue: Double
+	@Binding var game: Game
+
+	
+	var body: some View {
+		let roundedValue = Int(sliderValue.rounded())
+		let points = game.points(sliderValue: roundedValue)
+		VStack(spacing: 10) {
+			InstructionText(text: "The slider's value is")
+			BigNumberText(text: String(roundedValue))
+			BodyText(text: "You scored \(points) points!")
+			Button {
+				alertIsVisible = false
+				game.startNewRound(points: points)
+			} label: {
+				ButtonText(text: "Start New Round")
+			}
+		}
+			.padding()
+			.frame(maxWidth: 300)
+			.background(Color("BackgroundColor"))
+			.cornerRadius(21)
+			.shadow(radius: 10, y: 5)
+	}
+}
+
+struct PointsView_Previews: PreviewProvider {
+	static private var alertIsVisible = Binding.constant(false)
+	static private var sliderValue = Binding.constant(50.0)
+	static private var game = Binding.constant(Game())
+	
+	static var previews: some View {
+		PointsView(alertIsVisible: alertIsVisible, sliderValue: sliderValue, game: game)
+		PointsView(alertIsVisible: alertIsVisible, sliderValue: sliderValue, game: game)
+			.previewInterfaceOrientation(.landscapeRight)
+			.preferredColorScheme(.dark)
+	}
+}
